a77c4e65db7887163e187738b5edf05183a2fcef
通晓宇宙 <2581011320@qq.com>
2022-07-13 13:10:12 +0800

    验证码模块

--- smms/src/main/java/com/xftxyz/smms/App.java
+++ smms/src/main/java/com/xftxyz/smms/App.java
@@ -3,14 +3,17 @@ package com.xftxyz.smms;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.Map;
 
+import com.xftxyz.smms.utils.CodeUtil;
 import com.xftxyz.smms.utils.JDBCUtil;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.control.Alert;
-import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 /**
@@ -20,22 +23,26 @@ public class App extends Application {
 
     private static Connection conn;
 
+    public void setImage(ImageView imageView) {
+        Map<String, Object> map = CodeUtil.get();
+        System.out.println(map.get("code"));
+        imageView.setImage((Image) map.get("image"));
+    }
+
     @Override
     public void start(Stage primaryStage) throws Exception {
+        VBox vBox = new VBox();
+        ImageView imageView = new ImageView();
+        setImage(imageView);
         Button btn = new Button("Test");
         btn.setOnAction(e -> {
-            Alert alert = new Alert(AlertType.INFORMATION);
-            alert.setTitle("Information Dialog");
-            alert.setHeaderText("Look, an Information Dialog");
-            alert.setContentText("I have a great message for you!");
-
-            alert.showAndWait();
+            setImage(imageView);
         });
-        Scene scene = new Scene(btn, 640, 480);
+        vBox.getChildren().addAll(imageView, btn);
+        Scene scene = new Scene(vBox, 640, 480);
         primaryStage.setScene(scene);
         primaryStage.setTitle("XX超市管理系统");
         primaryStage.show();
-        // System.out.println(7890);
     }
 
     public static void main(String[] args) {
@@ -51,82 +58,5 @@ public class App extends Application {
         launch(args);
         JDBCUtil.close(conn);
     }
-
-    // System.out.println(123);
-    // private static Scene scene;
-    // @Override
-    // public void start(Stage stage) throws IOException {
-    // // try {
-    // // Connection connection = JDBCUtils.getConnection();
-    // // System.out.println(connection);
-    // // } catch (ClassNotFoundException | SQLException | IOException e) {
-    // // e.printStackTrace();
-    // // }
-    // // scene = new Scene(loadFXML("primary"), 640, 480);
-    // // stage.setScene(scene);
-    // Button b1 = new Button("get");
-    // b1.setOnAction(e -> {
-    // try {
-    // Connection connection = JDBCUtils.getConnection();
-    // UserDao userDao = new UserDaoImpl();
-    // User user = new User();
-    // user.setName("admin");
-    // user.setPwd("admin888");
-    // User user2 = userDao.getUser(connection, user.getName(), user.getPwd());
-    // System.out.println(user2);
-    // JDBCUtils.close(connection);
-    // } catch (ClassNotFoundException | SQLException | IOException e1) {
-    // e1.printStackTrace();
-    // }
-    // });
-    // Button b2 = new Button("add");
-    // b2.setOnAction(e -> {
-    // try {
-    // Connection connection = JDBCUtils.getConnection();
-    // UserDao userDao = new UserDaoImpl();
-    // User user = new User();
-    // user.setName("admin");
-    // user.setPwd("admin888");
-    // user.setLimits("admin");
-    // user.setCreateAt(Timestamp.valueOf(LocalDateTime.now()));
-    // userDao.saveUser(connection, user);
-    // System.out.println(user);
-    // JDBCUtils.close(connection);
-    // } catch (ClassNotFoundException | SQLException | IOException e1) {
-    // e1.printStackTrace();
-    // }
-    // });
-    // // StackPane stackPanel = new StackPane();
-    // VBox vbox = new VBox();
-    // vbox.getChildren().add(b1);
-    // vbox.getChildren().add(b2);
-    // Scene scene = new Scene(vbox, 640, 480);
-    // stage.setScene(scene);
-    // stage.setTitle("超市管理系统");
-    // stage.show();
-    // }
-
-    // static void setRoot(String fxml) throws IOException {
-    // scene.setRoot(loadFXML(fxml));
-    // }
-
-    // private static Parent loadFXML(String fxml) throws IOException {
-    // FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml +
-    // ".fxml"));
-    // return fxmlLoader.load();
-    // }
-
-    // public static void main(String[] args) {
-    // launch();
-    // InputStream resourceAsStream = App.class.getResourceAsStream("primary.fxml");
-    // System.out.println(resourceAsStream);
-    // try {
-    // byte[] b = new byte[resourceAsStream.available()];
-    // resourceAsStream.read(b);
-    // System.out.println(new String(b));
-    // } catch (IOException e) {
-    // e.printStackTrace();
-    // }
-    // }
 
 }
--- smms/src/main/java/com/xftxyz/smms/utils/CodeUtil.java
+++ smms/src/main/java/com/xftxyz/smms/utils/CodeUtil.java
@@ -1,109 +1,71 @@
 package com.xftxyz.smms.utils;
 
 import java.awt.BasicStroke;
-import java.awt.Color;
-import java.awt.Font;
 import java.awt.Graphics2D;
 import java.awt.RenderingHints;
 import java.awt.geom.QuadCurve2D;
 import java.awt.image.BufferedImage;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.OutputStream;
-import java.util.Random;
+import java.util.Map;
 
-import javax.imageio.ImageIO;
+import javafx.scene.image.Image;
 
 public class CodeUtil {
-    private final static Random random = new Random();
-    // 随机字体样式
-    private final static int[] fontStyles = { Font.HANGING_BASELINE, Font.ITALIC, Font.LAYOUT_LEFT_TO_RIGHT,
-            Font.LAYOUT_NO_LIMIT_CONTEXT, Font.LAYOUT_NO_START_CONTEXT,
-            Font.LAYOUT_RIGHT_TO_LEFT, Font.CENTER_BASELINE, Font.PLAIN, Font.ROMAN_BASELINE, Font.TRUETYPE_FONT,
-            Font.TYPE1_FONT, Font.BOLD };
 
-    private final static String[] fonts = { "宋体", "华文楷体", "黑体", "微软雅黑" };
-    private final static char[] c = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
-                                      'j', 'k', 'm', 'n', 'p', 'q', 'r', 's',
-                                      't', 'u', 'v', 'w', 'x', 'y', 'z', 
-                                      'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'J', 'K', 'L', 'M', 'N', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '1', '3', '5',
-            '6', '7', '8', '9' };
-
-    private final static int[] xarr = { 23, 24, 19, 20, 21 };
-    private final static int[] yarr = { 20, 21, 22, 23, 24, 25, 16, 17, 18 };
-    private final static int[] font_size = { 28, 29, 30, 21, 22, 23, 24, 27, 26, 25 };
+    private static final String codechars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
 
     // 画随机码图
-    public void draw(OutputStream out, String value) throws IOException {
+    private static BufferedImage draw(String value) {
         int width = 110, height = 40;
         BufferedImage bi = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
         Graphics2D g = (Graphics2D) bi.getGraphics();
         g.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-        g.setColor(randColor(240, 255));
+        g.setColor(RandomUtil.getColor(240, 255));
         g.fillRect(0, 0, width, height);
         g.drawRect(1, 1, width - 2, height - 2);
         for (int i = 0; i < 10; i++) {
-            g.setColor(randColor(150, 250));
-            g.drawOval(random.nextInt(110), random.nextInt(24), 5 + random.nextInt(10), 5 + random.nextInt(10));
+            g.setColor(RandomUtil.getColor(150, 250));
+            g.drawOval(RandomUtil.getInt(0, width), RandomUtil.getInt(0, 24),
+                    RandomUtil.getInt(5, 10), RandomUtil.getInt(5, 10));
         }
-        g.setFont(getFont());
-        g.setColor(randColor(xarr[(int) (Math.random() * 5)], 254));
+        g.setFont(RandomUtil.getFont());
+        g.setColor(RandomUtil.getColor(RandomUtil.getInt(19, 25), 254));
         for (int i = 0, len = value.length(); i < len; i++) {
             String rand = String.valueOf(value.charAt(i));
-            int degree = random.nextInt(23);
-            if (i % 2 == 0) {
-                degree = degree * (-1);
-            }
-            int x = xarr[(int) (Math.random() * 5)] * i, y = yarr[(int) (Math.random() * 8)];
+            int degree = RandomUtil.getInt(0, 23);
+            degree = i % 2 == 0 ? -degree : degree;
+            int x = RandomUtil.getInt(19, 25) * i;
+            int y = RandomUtil.getInt(16, 25);
             g.rotate(Math.toRadians(degree), x, y);
-            g.setColor(randColor(48, 254));
+            g.setColor(RandomUtil.getColor(48, 254));
             g.drawString(rand, x + 8, y + 10);
             g.rotate(-Math.toRadians(degree), x, y);
         }
         // 图片中间线
-        g.setColor(randColor(0, 200));
-        // width是线宽,float型
+        g.setColor(RandomUtil.getColor(0, 200));
+        // width是线宽, float型
         BasicStroke bs = new BasicStroke(2);
         g.setStroke(bs);
         // 画出曲线
-        QuadCurve2D.Double curve = new QuadCurve2D.Double(0d, random.nextInt(height - 8) + 4, width / 2, height / 2,
-                width, random.nextInt(height - 8) + 4);
+        QuadCurve2D.Double curve = new QuadCurve2D.Double(0d,
+                RandomUtil.getInt(4, height - 8), width / 2, height / 2, width,
+                RandomUtil.getInt(4, height - 8));
         g.draw(curve);
         // 销毁图像
         g.dispose();
-        ImageIO.write(bi, "png", out);
+        return bi;
     }
 
-    private Font getFont() {
-        return new Font(fonts[(int) (Math.random() * 4)], fontStyles[(int) (Math.random() * 12)],
-                font_size[(int) (Math.random() * 10)]);
-    }
+    public static Map<String, Object> get() {
+        String code = RandomUtil.choice(codechars, 4);
+        BufferedImage bi = draw(code);
+        Image img = null;
+        try {
+            img = ImageConverter.awt2jfx(bi);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return Map.of("code", code, "image", img);
 
-    private Color randColor(int fc, int bc) {// 给定范围获得随机颜色
-        if (fc > 255) {
-            fc = 255;
-        }
-        if (bc > 255) {
-            bc = 255;
-        }
-        int r = fc + random.nextInt(bc - fc);
-        int g = fc + random.nextInt(bc - fc);
-        int b = fc + random.nextInt(bc - fc);
-        return new Color(r, g, b);
-    }
-
-    public String getRandom(int size) {
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < size; i++) {
-            sb.append(c[Math.abs(random.nextInt()) % c.length]);
-        }
-        return sb.toString();
-    }
-
-    public void main(String[] args) throws IOException {
-        for (int i = 1; i < 2; i++) {
-            FileOutputStream out = new FileOutputStream("1" + i + ".png");
-            draw(out, getRandom(4));
-        }
     }
 }
