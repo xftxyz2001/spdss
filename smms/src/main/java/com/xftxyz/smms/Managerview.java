b3c943c61186f4ebd4bec1b565d5c5a5387ae0e7
童年 <1346802467@qq.com>
2022-07-14 19:57:28 +0800

    登录、总经理

---
+++ smms/src/main/java/com/xftxyz/smms/Managerview.java
@@ -0,0 +1,175 @@
+package com.xftxyz.smms;
+
+
+
+
+
+
+
+
+
+import com.xftxyz.smms.entity.Goods;
+import com.xftxyz.smms.entity.User;
+
+import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.image.Image;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+public class Managerview extends Application {
+   public static void main(String[] args) {
+	launch(args);
+}
+
+
+public void insert1(Text text,AnchorPane ap) {
+	ap.getChildren().add(text);
+}
+
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		// TODO Auto-generated method stub
+		
+		Button usermanage = new Button("用户管理");
+		usermanage.setLayoutX(0);
+		usermanage.setLayoutY(150);
+		usermanage.setPrefHeight(80);
+		usermanage.setPrefWidth(200);
+		AnchorPane ap = new AnchorPane();
+		AnchorPane apleft = new AnchorPane();
+		AnchorPane aprightuser = new AnchorPane();
+		AnchorPane aprightgoods = new AnchorPane();
+		AnchorPane apuser = new AnchorPane();
+		
+		AnchorPane apgoods = new AnchorPane();
+		usermanage.setOnAction(e -> {
+		    apgoods.setVisible(false);
+		    apuser.setVisible(true);
+            });
+	
+	
+		Button goodsmanage = new Button("商品管理");
+		goodsmanage.setLayoutX(0);
+		goodsmanage.setLayoutY(300);
+		goodsmanage.setPrefHeight(80);
+		goodsmanage.setPrefWidth(200);
+		goodsmanage.setOnAction(e -> {
+		    apgoods.setVisible(true);
+		    apuser.setVisible(false);
+            });
+		ap.getChildren().addAll(apleft,apuser,apgoods);
+		apleft.setPrefHeight(600);
+		apleft.setPrefWidth(200);
+		apleft.setStyle("-fx-background-color:#708090");
+		apuser.getChildren().add(aprightuser);
+		apgoods.getChildren().add(aprightgoods);
+		apuser.setTopAnchor(aprightuser,0.0);
+		apuser.setRightAnchor(aprightuser,0.0);
+		apuser.setLeftAnchor(aprightuser, 0.0);
+		apuser.setBottomAnchor(aprightuser, 500.0);
+		apgoods.setTopAnchor(aprightgoods,0.0);
+		apgoods.setRightAnchor(aprightgoods,0.0);
+		apgoods.setLeftAnchor(aprightgoods, 0.0);
+		apgoods.setBottomAnchor(aprightgoods, 500.0);
+		ap.setTopAnchor(apuser,0.0);
+		ap.setRightAnchor(apuser,0.0);
+		ap.setLeftAnchor(apuser, 200.0);
+		ap.setBottomAnchor(apuser, 0.0);
+		ap.setTopAnchor(apgoods,0.0);
+		ap.setRightAnchor(apgoods,0.0);
+		ap.setLeftAnchor(apgoods, 200.0);
+		ap.setBottomAnchor(apgoods, 0.0);
+		aprightuser.setStyle("-fx-background-color:#696969");
+		aprightgoods.setStyle("-fx-background-color:#696969");
+		apleft.getChildren().addAll(goodsmanage,usermanage);
+		Scene scene = new Scene(ap);
+		
+		primaryStage.setScene(scene);
+		primaryStage.setHeight(600);
+		primaryStage.setWidth(800);
+		primaryStage.setTitle("欢迎使用");
+		primaryStage.setResizable(false);
+		ObservableList<User>list=FXCollections.observableArrayList();
+		Label usermagelabel = new Label("用户管理");
+        usermagelabel.setFont(Font.font("FangSong", FontWeight.BOLD, 20));
+        usermagelabel.setTextFill(Color.BLACK);
+        TableView <User> tableview = new TableView<User>(list);
+        TableColumn<User, String> user_name =new TableColumn<User,String>("姓名");
+        TableColumn<User, String> user_id =new TableColumn<User,String>("id");
+        TableColumn<User, String> user_pwd =new TableColumn<User,String>("密码");
+        TableColumn<User, String> user_time =new TableColumn<User,String>("创建时间");
+        tableview.getColumns().addAll(user_id,user_name,user_pwd,user_time);
+        
+       
+        usermagelabel.setLayoutX(230);
+        usermagelabel.setLayoutY(30);
+        tableview.setLayoutX(100);
+        tableview.setLayoutY(100);
+
+       aprightuser.getChildren().add(usermagelabel);
+       aprightuser.getChildren().add(tableview);
+       Button createbt = new Button("创建用户");
+       Button deletebt = new Button("删除用户");
+       Button changebt =new Button("修改信息");
+       createbt.setLayoutX(450);
+       createbt.setLayoutY(140);
+       deletebt.setLayoutX(450);
+       deletebt.setLayoutY(220);
+       changebt.setLayoutX(450);
+       changebt.setLayoutY(300);
+       aprightuser.getChildren().addAll(createbt,deletebt,changebt);
+       ObservableList<Goods>list1=FXCollections.observableArrayList();
+		Label goodsmageLabel = new Label("商品管理");
+       goodsmageLabel.setFont(Font.font("FangSong", FontWeight.BOLD, 20));
+       goodsmageLabel.setTextFill(Color.BLACK);
+       TableView <Goods> tableview1 = new TableView<Goods>(list1);
+       TableColumn<Goods, String> goods_name =new TableColumn<Goods,String>("商品名");
+       TableColumn<Goods, String> goods_id =new TableColumn<Goods,String>("id");
+       TableColumn<Goods, String> goods_price =new TableColumn<Goods,String>("价格");
+       TableColumn<Goods, String> goods_category =new TableColumn<Goods,String>("分类");
+       TableColumn<Goods, String> goods_number =new TableColumn<Goods,String>("数量");
+       TableColumn<Goods, String> goods_unit =new TableColumn<Goods,String>("单位");
+       TableColumn<Goods, String> goods_onsell =new TableColumn<Goods,String>("在售");
+       
+
+       tableview1.getColumns().addAll(goods_category,goods_id,goods_name,goods_number,goods_onsell,goods_price,goods_unit);
+       
+      
+       goodsmageLabel.setLayoutX(230);
+       goodsmageLabel.setLayoutY(30);
+       tableview1.setLayoutX(10);
+       tableview1.setLayoutY(100);
+
+      aprightgoods.getChildren().add(goodsmageLabel);
+      aprightgoods.getChildren().add(tableview1);
+      Button importbt = new Button("进货表");
+      Button remainbt = new Button("库存");
+      Button sellbt =new Button("销售表");
+      Button derivebt= new Button("导出");
+     importbt.setLayoutX(60);
+      importbt.setLayoutY(520);
+      remainbt.setLayoutX(170);
+      remainbt.setLayoutY(520);
+      sellbt.setLayoutX(280);
+      sellbt.setLayoutY(520);
+      derivebt.setLayoutX(390);
+      derivebt.setLayoutY(520);
+      aprightgoods.getChildren().addAll(importbt,remainbt,sellbt,derivebt);
+      apgoods.setVisible(false);
+        primaryStage.show();
+    	primaryStage.getIcons().add(new Image("file:/D:/sp111/spds/smms/src/main/java/com/xftxyz/smms/view/th(1).png"));
+	}
+
+}
---
+++ smms/src/main/java/com/xftxyz/smms/Register.java
@@ -0,0 +1,104 @@
+package com.xftxyz.smms;
+
+import java.util.Map;
+
+import com.xftxyz.smms.utils.CodeUtil;
+
+import javafx.application.Application;
+import javafx.event.EventHandler;
+import javafx.geometry.Pos;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.Scene;
+import javafx.scene.chart.BubbleChart;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+//登录界面
+public class Register extends Application{
+	public static void main(String[] args) {
+		launch(args);
+	}
+	 public void setImage(ImageView imageView) {
+	        Map<String, Object> map = CodeUtil.get();
+	        //System.out.println(map.get("code"));
+	        imageView.setImage((Image) map.get("image"));
+	    }
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		// TODO Auto-generated method stub
+		 ImageView imageView = new ImageView();
+	        setImage(imageView);
+	        imageView.setOnMouseClicked(new EventHandler<MouseEvent>() {
+
+				@Override
+				public void handle(MouseEvent event) {
+					// TODO Auto-generated method stub
+					  setImage(imageView);
+				}
+			});
+	        imageView.setLayoutX(220);
+	        imageView.setLayoutY(160);
+	        imageView.setFitWidth(80);
+	        imageView.setFitHeight(25);
+		AnchorPane ap = new AnchorPane();//网格布局类，作为根节点
+		ap.setStyle("-fx-background-color:#FFFAF0");//设置背景色
+		Scene scene = new Scene(ap);//设置场景
+		Text title = new Text("用户登录");
+		title.setFont(Font.font("FangSong", FontWeight.NORMAL, 20));
+		title.setFill(Color.CYAN);
+		Label Account = new Label("账号");
+		Account.setFont(Font.font("FangSong", FontWeight.NORMAL, 15));
+		Label Pwd = new Label("密码");
+		Pwd.setFont(Font.font("FangSong", FontWeight.NORMAL, 15));
+		Label Idf = new Label("验证码");
+		Idf.setFont(Font.font("FangSong", FontWeight.NORMAL, 15));
+		TextField Accounttextfield = new TextField();
+		TextField identifytextfield = new TextField();
+		Button rgsbutton = new Button("登录");
+		rgsbutton.setFont(Font.font("FangSong", FontWeight.NORMAL, 15));
+     	rgsbutton.setStyle("-fx-background-color:#6495ED");
+     	rgsbutton.setPrefWidth(220);
+		rgsbutton.setLayoutX(100);
+		rgsbutton.setLayoutY(200);
+		identifytextfield.setPrefWidth(60); 
+		PasswordField passwordfield = new PasswordField();
+		ap.getChildren().addAll(title,Account,Pwd,Idf,Accounttextfield,identifytextfield,passwordfield,rgsbutton,imageView);
+		title.setLayoutX(100);
+		title.setLayoutY(80);
+		Account.setLayoutX(110);
+		Account.setLayoutY(100);
+		Pwd.setLayoutX(110);
+		Pwd.setLayoutY(130);
+		Idf.setLayoutX(95);
+		Idf.setLayoutY(165);
+		Accounttextfield.setLayoutX(150);
+		Accounttextfield.setLayoutY(100);
+		identifytextfield.setLayoutX(150);
+		identifytextfield.setLayoutY(160);
+		passwordfield.setLayoutX(150);
+		passwordfield.setLayoutY(130);
+		primaryStage.setScene(scene);
+		primaryStage.setHeight(350);
+		primaryStage.setWidth(400);
+		primaryStage.setTitle("用户登录");
+		primaryStage.setResizable(false);
+		primaryStage.getIcons().add(new Image("file:/D:/sp111/spds/smms/src/main/java/com/xftxyz/smms/view/register.png"));
+	    
+	    
+        primaryStage.show();
+		
+
+	}
+
+}
